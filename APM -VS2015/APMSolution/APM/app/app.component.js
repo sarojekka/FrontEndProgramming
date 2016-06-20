0c02d9600013f7a6eabf5f7bb28032f5ce68e28b
Ekka <saroj.ekka@hpe.com>
2016-06-20 03:09:35 -0700

    product folder exercise

--- APM -VS2015/APMSolution/APM/app/app.component.js
+++ APM -VS2015/APMSolution/APM/app/app.component.js
@@ -1,13 +1,38 @@
-System.register([], function(exports_1, context_1) {
+System.register(['angular2/core', './products/product-list.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, product_list_component_1;
     var AppComponent;
     return {
-        setters:[],
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (product_list_component_1_1) {
+                product_list_component_1 = product_list_component_1_1;
+            }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.pageTitle = 'Saroj Acme Product Managemnet';
                 }
+                AppComponent = __decorate([
+                    core_1.Component({
+                        selector: 'pm-app',
+                        template: "\n    <div>\n        <h1>{{pageTitle}}</h1>\n        <pm-products>Loading Product List.... </pm-products>\n    </div>\n    ",
+                        directives: [product_list_component_1.ProductListComponent]
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
--- APM -VS2015/APMSolution/APM/app/app.component.ts
+++ APM -VS2015/APMSolution/APM/app/app.component.ts
@@ -1,2 +1,16 @@
-﻿export class AppComponent {
+﻿import {Component} from 'angular2/core';
+import {ProductListComponent} from './products/product-list.component';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+    <div>
+        <h1>{{pageTitle}}</h1>
+        <pm-products>Loading Product List.... </pm-products>
+    </div>
+    `,
+    directives: [ProductListComponent] 
+})
+export class AppComponent {
+    pageTitle:String = 'Saroj Acme Product Managemnet';
 }
