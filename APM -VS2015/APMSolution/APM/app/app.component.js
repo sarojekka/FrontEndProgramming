baee7b108292c0975f2ce1b6d554fda991959a9c
Ekka <saroj.ekka@hpe.com>
2016-06-19 21:56:54 -0700

    no message

---
+++ APM -VS2015/APMSolution/APM/app/app.component.js
@@ -0,0 +1,17 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var AppComponent;
+    return {
+        setters:[],
+        execute: function() {
+            AppComponent = (function () {
+                function AppComponent() {
+                }
+                return AppComponent;
+            }());
+            exports_1("AppComponent", AppComponent);
+        }
+    }
+});
+//# sourceMappingURL=app.component.js.map
